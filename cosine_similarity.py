3ab85fb70cfa6faf884aa5c66940fdc6f041709a
gcnTo <60984509+gcnTo@users.noreply.github.com>
2020-12-11 21:30:19 +0300

    Update cosine_similarity.py

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,7 +1,7 @@
 """
 COSINE SIMILARITY
 """
-
+# import numpy
 def cos_sim(d1,d2):
 	dot_product = 0
 	magnitude_d1 = 0
@@ -9,7 +9,8 @@ def cos_sim(d1,d2):
 	for i,j in zip(d1,d2):
 		magnitude_d1 += i**2
 		magnitude_d2 += j**2
-		dot_product += i*j
+		dot_product += i*j # OR YOU CAN USE 
+	# dot_product = numpy.dot(d1,d2)
 	return dot_product / (magnitude_d1**0.5 * magnitude_d2**0.5)
 
 """
